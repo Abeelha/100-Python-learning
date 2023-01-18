bf93736cc24568378d26789dcd2e0c6eaa322ce0
Abeelha <dev.theodoro@gmail.com>
2023-01-18 20:42:38 -0300

    Finished first day! :D

---
+++ Day 1/Final Project Day 1.py
@@ -0,0 +1,9 @@
+# Theodoro Bertol Dev (Abeelha) #
+# || Day 1 of #100DaysOfCode || #
+
+print("Hello! i would like to make a band name for you :)")
+
+city = input("Inform me the name of the city you were born:\n")
+pet = input("Inform me the name of the first pet you had:\n")
+
+print("The name of the band shall be " + city + " "+ pet + "!!!")
