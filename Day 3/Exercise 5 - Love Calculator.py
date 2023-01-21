b73539fd00ef736a7ee83b7abafd5b598e2db408
Abeelha <dev.theodoro@gmail.com>
2023-01-21 02:28:43 -0300

    Done

--- Day 3/Exercise 5 - Love Calculator.py
+++ Day 3/Exercise 5 - Love Calculator.py
@@ -1,2 +1,90 @@
 # Theodoro Bertol Dev (Abeelha) #
 # || Day 3 of #100DaysOfCode || #
+
+
+# ake both people's names and check for the number of times the letters in the word TRUE occurs. 
+
+# Then check for the number of times the letters in the word LOVE occurs. 
+
+# Then combine these numbers to make a 2 digit number.
+
+# For Love Scores less than 10 or greater than 90, the message should be:
+
+# "Your score is **x**, you go together like coke and mentos."
+# For Love Scores between 40 and 50, the message should be:
+
+# "Your score is **y**, you are alright together."
+# Otherwise, the message will just be their score. e.g.:
+
+# "Your score is **z**."
+# e.g.
+
+# name1 = "Angela Yu"
+# name2 = "Jack Bauer"
+# T occurs 0 times
+
+# R occurs 1 time
+
+# U occurs 2 times
+
+# E occurs 2 times
+
+# Total = 5
+
+# L occurs 1 time
+
+# O occurs 0 times
+
+# V occurs 0 times
+
+# E occurs 2 times
+
+# Total = 3
+
+# Love Score = 53
+
+# Print: "Your score is 53."
+
+# Example Input 1
+# name1 = "Kanye West"
+# name2 = "Kim Kardashian"
+# Example Output 1
+# Your score is 42, you are alright together.
+
+
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+
+#Write your code below this line 👇
+
+combined_string = name1 + name2
+name_low_case = combined_string.lower()
+
+
+t = name_low_case.count("t")
+r = name_low_case.count("r")
+u = name_low_case.count("u")
+e = name_low_case.count("e")
+
+l = name_low_case.count("l")
+o = name_low_case.count("o")
+v = name_low_case.count("v")
+e = name_low_case.count("e")
+
+true = t + r + u + e
+love = l + o + v + e
+
+love_score = int(str(true) + str(love))
+
+if love_score < 10 or love_score > 90:
+    print(f"Your score is {love_score}, you go together like coke and mentos.")
+elif love_score >= 40 and love_score <= 50:
+    print(f"Your score is {love_score}, you are alright together.")
+else:
+    print(f"Your score is {love_score}.")
+
+
+
+
+
