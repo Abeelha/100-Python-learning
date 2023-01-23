a6fb8fa0431d549da0d1b6172105baf83acb0966
Abeelha <dev.theodoro@gmail.com>
2023-01-23 13:02:27 -0300

    Changed some of the code for the bee minigame <3

--- Day 3/Final project Day 3 Treasure Island.py
+++ Day 3/Final project Day 3 Treasure Island.py
@@ -80,20 +80,6 @@ elif low_tree_river == "river":
           """)
     noise_sound = input("\nNow you can hear a loud noise, will you follow it or ignore it?\nFollow / Ignore: ")
     low_noise_sound = noise_sound.lower()
-    print(r"""
-                                  ...vvvv)))))).
-       /~~\               ,,,c(((((((((((((((((/
-      /~~c \.         .vv)))))))))))))))))))\``
-          G_G__   ,,(((KKKK//////////////'
-        ,Z~__ '@,gW@@AKXX~MW,gmmmz==m_.
-       iP,dW@!,A@@@@@@@@@@@@@@@A` ,W@@A\c
-       ]b_.__zf !P~@@@@@*P~b.~+=m@@@*~ g@Ws.
-          ~`    ,2W2m. '\[ ['~~c'M7 _gW@@A`'s
-            v=XX)====Y-  [ [    \c/*@@@*~ g@@i
-           /v~           !.!.     '\c7+sg@@@@@s.
-          //              'c'c       '\c7*X7~~~~
-         ]/                 ~=Xm_       '~=(Gm_.
-          """)
     if low_noise_sound == "follow":
         print("\nOh no! the loud noise was actualy a Human with a chainsaw!!!")
         print(r"""
@@ -122,8 +108,22 @@ elif low_tree_river == "river":
               """)
         print("\nThe human killed you, Game over")
     elif low_noise_sound == "ignore":
-        pollen_Y_N = input("\nNow you can see a bee on the ground, she's weak and need some pollen, you stil got some from the flower from before\n give the pollen to her?\nYes / No: ")
+        pollen_Y_N = input("\nNow you can see a bee on the ground, she's weak and need some pollen, you stil got some from the flower before\n give the pollen to her?\nYes / No: ")
         low_pollen_Y_N = pollen_Y_N.lower()
+        print(r"""
+                                  ...vvvv)))))).
+       /~~\               ,,,c(((((((((((((((((/
+      /~~c \.         .vv)))))))))))))))))))\``
+          G_G__   ,,(((KKKK//////////////'
+        ,Z~__ '@,gW@@AKXX~MW,gmmmz==m_.
+       iP,dW@!,A@@@@@@@@@@@@@@@A` ,W@@A\c
+       ]b_.__zf !P~@@@@@*P~b.~+=m@@@*~ g@Ws.
+          ~`    ,2W2m. '\[ ['~~c'M7 _gW@@A`'s
+            v=XX)====Y-  [ [    \c/*@@@*~ g@@i
+           /v~           !.!.     '\c7+sg@@@@@s.
+          //              'c'c       '\c7*X7~~~~
+         ]/                 ~=Xm_       '~=(Gm_.
+          """)
         if pollen_Y_N == "no":
             print("\nThe weak bee was a worker from your hive, but she died, now you dont know how to get back to the hive, because you didnt help her...")
             print(r"""
