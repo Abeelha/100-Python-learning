9c4c209499cb9a6a7466350a33d9419a3018bc7a
Abeelha <dev.theodoro@gmail.com>
2023-01-30 21:49:53 -0300

    Day 10 done with calculator project! :)

--- Day 10/Exercise 1 - Days in Month.py
+++ Day 10/Exercise 1 - Days in Month.py
@@ -6,18 +6,29 @@ def is_leap(year):
   if year % 4 == 0:
     if year % 100 == 0:
       if year % 400 == 0:
-        print("Leap year.")
+        return True
       else:
-        print("Not leap year.")
+        return False
     else:
-      print("Leap year.")
+      return True
   else:
-    print("Not leap year.")
+    return False
 
-def days_in_month():
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+# The List month_days contains the number of days in a month from January to December for a non-leap year.
+# A leap year has 29 days in February.
+def days_in_month(year, month):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
   
+  if month > 12 or month < 1:
+      return "Invalid Month"
   
+  call_leap_year = is_leap(year)
+  
+  if call_leap_year == True and month == 2:
+      return 29
+  else:
+      return month_days[month - 1]
+   
 #🚨 Do NOT change any of the code below 
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
