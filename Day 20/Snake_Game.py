7a3f7208de50b570fdc2e2ab5b23ef5b7a7068fe
Abeelha <dev.theodoro@gmail.com>
2023-02-10 10:57:56 -0300

    Added Comments in start snake game day 20

--- Day 20/Snake_Game.py
+++ Day 20/Snake_Game.py
@@ -6,26 +6,29 @@ import time
 import random
 from snake import Snake
 
+#Screen setup
 screen = Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
+#Snake setup
 snake = Snake()
 
+#Input keys to move snake
 screen.listen()
 screen.onkey(snake.up,"Up")
 screen.onkey(snake.down,"Down")
 screen.onkey(snake.left,"Left")
 screen.onkey(snake.right,"Right")
 
+#Make the game run
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
 
-    
-
+#Exit screen when clicking
 screen.exitonclick()
--- Day 20/snake.py
+++ Day 20/snake.py
@@ -1,8 +1,9 @@
 # Theodoro Bertol Dev (Abeelha) #
 # || Day 20 of #100DaysOfCode || #
 
-from tkinter import LEFT
+#Imports
 from turtle import Turtle
+#Const
 STARTING_POSITIONS =[(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
@@ -10,13 +11,14 @@ DOWN = 270
 LEFT = 180
 RIGHT = 0
 
+#Main class
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
 
-        
+    #Creates snake
     def create_snake(self):    
         for position in STARTING_POSITIONS:
             new_segment = Turtle(shape="square")
@@ -25,25 +27,30 @@ class Snake:
             new_segment.goto(position)
             self.segments.append(new_segment)
 
+    #Move snakle
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
-        
+    
+    #Move snake UP
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
-        
+    
+    #Move snake Down
     def down(self):
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
-        
+    
+    #Move snake left
     def left(self):
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
-        
+    
+    #Move snake right
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
