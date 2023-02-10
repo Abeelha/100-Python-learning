627c02e2ac5b75fb0b4a6a3c582d7618fb2f5efe
Abeelha <dev.theodoro@gmail.com>
2023-02-10 10:55:07 -0300

    Made starting Snake game! day 20

---
+++ Day 20/Snake_Game.py
@@ -0,0 +1,31 @@
+# Theodoro Bertol Dev (Abeelha) #
+# || Day 20 of #100DaysOfCode || #
+
+from turtle import Screen
+import time
+import random
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    
+
+screen.exitonclick()
