555a36e853d8373c15715cb5879a20f846ff9e82
Abeelha <dev.theodoro@gmail.com>
2023-02-12 16:33:32 -0300

    Day 22 Done, made pong game

---
+++ Day 22/ball.py
@@ -0,0 +1,33 @@
+# Theodoro Bertol Dev (Abeelha) #
+# || Day 22 of #100DaysOfCode || #
+
+#Imports
+from turtle import Turtle
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.shape("circle")
+        self.penup()
+        self.x_move = 3
+        self.y_move = 3
+        self.move_speed = 0.02
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        # self.move_speed *= 0.9
+
+    def reset_position(self):
+        self.goto(0, 0)
+        # self.move_speed = 0.002
+        self.bounce_x()
