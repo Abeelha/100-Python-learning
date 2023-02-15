5df38a09c55af149f0439835af930ca63afba2ea
Abeelha <dev.theodoro@gmail.com>
2023-02-14 22:46:21 -0300

    Day 24 Done

---
+++ Day 24/Snake game Updated/scoreboard.py
@@ -0,0 +1,38 @@
+# Theodoro Bertol Dev (Abeelha) #
+# || Day 24 of #100DaysOfCode || #
+
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("C:/Users/Abeelha/Documents/Programs/100 Python learning/Day 24/data.txt") as data:
+            self.high_score = int(data.read())
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("C:/Users/Abeelha/Documents/Programs/100 Python learning/Day 24/data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+
