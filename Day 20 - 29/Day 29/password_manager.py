c3032c8a58eb3a18fde4e87e0769070b6ec95fe3
Abeelha <dev.theodoro@gmail.com>
2023-02-26 16:35:37 -0300

    TODO done, encription and decription of password

--- Day 20 - 29/Day 29/password_manager.py
+++ Day 20 - 29/Day 29/password_manager.py
@@ -1,13 +1,15 @@
 # Theodoro Bertol Dev (Abeelha) #
 # || Day 29 of #100DaysOfCode || #
 
+# ---------------------------- IMPORTS ------------------------------- #
+
 from tkinter import *
 from tkinter import messagebox
-import hashlib
 from random import choice, randint, shuffle
-import pyperclip
+import csv, hashlib, pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -40,36 +42,35 @@ def save():
         message=f"These are the details entered: \nEmail: {email} "
         f"\nPassword: {password} \nIs it ok to save?",
     ):
-        # generate encrypted password:
-        #TODO try making this decode and incript password work
-        encode_pass = hashlib.sha256(password.encode())
-        password = encode_pass.hexdigest()
-        with open("C:/Users/Abeelha/Documents/Programs/100 Python learning/Day 20 - 29/Day 29/data.txt", "a") as data_file:
-            data_file.write(f"{website} | {email} | {password}\n")
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
-            
+        passwords(password, website, email)
 
-# ---------------------------- DECODE PASSWORD ------------------------------- # 
-#TODO try making this decode and incript password work  
-def decrypt_and_store():
-    # Get information from data.txt
-    with open('C:/Users/Abeelha/Documents/Programs/100 Python learning/Day 20 - 29/Day 29/data.txt', 'r') as data_file:
-        lines = data_file.readlines()
-        data_info = [line.strip().split(' | ') for line in lines]
-    
-    # Get the encryption key
-    encryption_key = input("Please enter your encryption key: ")
-    
-    # Decrypt passwords
-    decrypted_passwords = [decrypt(password, encryption_key) for website, _, password in data_info]
+##--------------SAVING FILES:--------------##
+def saving_in_file(data_file, website, email, arg3):
+    writer = csv.writer(data_file, lineterminator='\n')
+    writer.writerow([website, email, arg3])
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
 
-    # Store in new file
-    file_name = input("Please enter a file name to store the decrypted passwords: ")
-    with open('C:/Users/Abeelha/Documents/Programs/100 Python learning/Day 20 - 29/Day 29/{}.txt'.format(file_name), 'w+') as new_file:
-        for website, password in zip(data_info, decrypted_passwords):
-            website_entry, email_entry, encrypted_password = website
-            new_file.write(f'{website_entry} | {email_entry} | {password}\n')
+##--------------GENERATE ENCRYPTED PASSWORD:--------------##
+def passwords(password, website, email):
+
+    decoded_pass = password
+    encode_pass = hashlib.sha256(password.encode())
+    password = encode_pass.hexdigest()
+
+    ##-------------------- SAVING AS TXT: --------------------##
+
+    # with open("C:/Users/Abeelha/Documents/Programs/100 Python learning/Day 20 - 29/Day 29/data_coded.txt", "a") as data_file:
+    #     saving_in_file(data_file, website, email, password)
+    # with open("C:/Users/Abeelha/Documents/Programs/100 Python learning/Day 20 - 29/Day 29/data_decoded.txt", "a") as data_file:
+    #     saving_in_file(data_file, website, email, decoded_pass)
+    
+    ##-------------------- SAVING AS CSV: --------------------##
+
+    with open("C:/Users/Abeelha/Documents/Programs/100 Python learning/Day 20 - 29/Day 29/data_coded.csv", "a") as data_file:
+        saving_in_file(data_file, website, email, password)
+    with open("C:/Users/Abeelha/Documents/Programs/100 Python learning/Day 20 - 29/Day 29/data_decoded.csv", "a") as data_file:
+        saving_in_file(data_file, website, email, decoded_pass)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -97,7 +98,7 @@ website_entry.grid(row=1, column=1, columnspan=2)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "@gmail.com")
+email_entry.insert(0, "@gmail.com"),
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
@@ -109,8 +110,7 @@ add_button.grid(row=4, column=1, columnspan=2)
 
 
 # ---------------------------- Decrypt Password Button ------------------------------- #
-#TODO try making this decode and incript password work
-# decode_password_button = Button(text="Decrypt Password", command=decrypt_and_store)
+# decode_password_button = Button(text="Decrypt Password", command=decrypt_password)
 # decode_password_button.grid(row=5, column=1, columnspan=2)
 
 window.mainloop()
