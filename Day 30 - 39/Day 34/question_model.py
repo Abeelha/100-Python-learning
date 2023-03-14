f4ebde865c4f5ca449267ce321c9d06c031600bd
Abeelha <dev.theodoro@gmail.com>
2023-03-14 20:53:49 -0300

    Day 34 done :D

---
+++ Day 30 - 39/Day 34/question_model.py
@@ -0,0 +1,8 @@
+# Theodoro Bertol Dev (Abeelha) #
+# || Day 34 of #100DaysOfCode || #
+
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
