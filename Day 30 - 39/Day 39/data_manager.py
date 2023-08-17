f1508aa003d4a2c1e420782e4360dc0f13c0a917
Theodoro Bertol <dev.theodoro@gmail.com>
2023-08-17 03:54:06 -0300

    updated day 39

--- Day 30 - 39/Day 39/data_manager.py
+++ Day 30 - 39/Day 39/data_manager.py
@@ -6,8 +6,7 @@ import requests
 
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    GET_ENDPOINT_SHEETY = "https://api.sheety.co/7e1fc263972ddfa7d19da8424cfaca11/flightDeals/prices"
-    PUT_ENDPOINT_SHEETY = "https://api.sheety.co/7e1fc263972ddfa7d19da8424cfaca11/flightDeals/prices/[Object ID]"
+
     
     response_get = requests.get(url=GET_ENDPOINT_SHEETY)
     
--- Day 30 - 39/Day 39/notification_manager.py
+++ Day 30 - 39/Day 39/notification_manager.py
@@ -5,10 +5,7 @@ from twilio.rest import Client
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
 
-    # Find your Account SID and Auth Token at twilio.com/console
-    # and set the environment variables. See http://twil.io/secure
-    account_sid = "AC7bcdc60e5c4b5cdb08c55dd62a78348b"
-    auth_token = "52328101fe6218689f5e8a2efb2c8dd2"
+
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
