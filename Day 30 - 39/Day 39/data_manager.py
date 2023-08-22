8765044b3cad83a35d697c6ff099b4a0ab920115
Theodoro Bertol <dev.theodoro@gmail.com>
2023-08-22 09:59:51 -0300

    First commit day 39

--- Day 30 - 39/Day 39/data_manager.py
+++ Day 30 - 39/Day 39/data_manager.py
@@ -1,28 +1,41 @@
 # Theodoro Bertol Dev (Abeelha) #
 # || Day 39 of #100DaysOfCode || #
 
-import os
-from dotenv import load_dotenv, dotenv_values
-load_dotenv()
-
-from pprint import pprint
-import requests
-
-
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
+    import os
+    from dotenv import load_dotenv, dotenv_values
+    from pprint import pprint
+    import requests
+    load_dotenv()
 
+    # This class is responsible for talking to the Google Sheet.
+    GET_ENDPOINT_SHEETY = os.getenv("day39_GET_ENDPOINT_SHEETY")
+    # print(GET_ENDPOINT_SHEETY)
+    HEADER_AUTH_SHEETY = os.getenv("day39_HEADER_SHEETY")
+    # print(HEADER_AUTH_SHEETY)
     
-    response_get = requests.get(url=GET_ENDPOINT_SHEETY)
-    
-    
-    json_data = response_get.json()
-    
-    for prices in json_data["prices"]:
-        sheet_data = {
-            "price": prices["Lowest Price"],
-        }
 
-    
-    pprint(response_get)
+    # Check if environment variables are set
+    if GET_ENDPOINT_SHEETY is None or HEADER_AUTH_SHEETY is None:
+        raise ValueError("Environment variables are not set.")
+        
+    try:
+        # Make the GET request
+        response_get = requests.get(url=GET_ENDPOINT_SHEETY, headers={"Authorization": HEADER_AUTH_SHEETY})
+        # pprint(response_get.text)
+
+        # # Check response status code
+        if response_get.status_code == 200:
+            json_data = response_get.json()
+            
+            for prices in json_data["prices"]:
+                sheet_data = {
+                    "price": prices["lowestPrice"],
+                }
+                pprint(sheet_data)
+        else:
+            print("Request failed with status code:", response_get.status_code)
+
+    except requests.RequestException as e:
+        print("Request error:", e)
     pass
--- Day 30 - 39/Day 39/notification_manager.py
+++ Day 30 - 39/Day 39/notification_manager.py
@@ -1,22 +1,32 @@
 # Theodoro Bertol Dev (Abeelha) #
 # || Day 39 of #100DaysOfCode || #
 
-import os
-from dotenv import load_dotenv, dotenv_values
-load_dotenv()
 
-from twilio.rest import Client
 
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
+    import os
+    import requests
+    from twilio.rest import Client
+    from dotenv import load_dotenv, dotenv_values
+    load_dotenv()
 
+    ACCOUNT_SID = os.getenv("day39_account_sid")
+    AUTH_TOKEN = os.getenv("day39_auth_token")
+    WHATS_API_URL = os.getenv("day39_WHATS_API")
+    from_whats = os.getenv("day39_from_whats")
+    to_whats = os.getenv("day39_to_whats")
 
-    client = Client(account_sid, auth_token)
+    account_sid = 'AC7bcdc60e5c4b5cdb08c55dd62a78348b'
+    auth_token = '[AuthToken]'
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
     message = client.messages.create(
-        body='Hello there!',
         from_='whatsapp:+14155238886',
-        to='whatsapp:+15005550006'
+        body='Your appointment is coming up on July 21 at 3PM',
+        to='whatsapp:+555191068594'
     )
+
     print(message.sid)
+    
     pass
