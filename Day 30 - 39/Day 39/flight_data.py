8f21c87d8ad7bb5487c03fccfbe05303953b7ffa
Theodoro Bertol <dev.theodoro@gmail.com>
2023-08-24 14:22:37 -0300

    first commit implementing flight search

--- Day 30 - 39/Day 39/flight_data.py
+++ Day 30 - 39/Day 39/flight_data.py
@@ -3,12 +3,60 @@
 
 import os
 import requests
+from datetime import datetime, timedelta
 from dotenv import load_dotenv, dotenv_values
 from data_manager import DataManager
+from pprint import pprint
 
-# load_dotenv()
-# header_key = {
-#     "apikey" : API_KEY_TEQUILA
-# }
+TEQUILA_ENDPOINT = os.getenv("TEQUILA_ENDPOINT")
+API_KEY_TEQUILA = os.getenv("day39_APIKEY_Tequila")
 
-# # class FlightData:
+header_key = {
+    "apikey" : API_KEY_TEQUILA
+}
+
+class FlightData:
+
+    def __init__(self, price, origin_city, origin_airport, destination_city, destination_airport, out_date, return_date):
+        self.price = price
+        self.origin_city = origin_city
+        self.origin_airport = origin_airport
+        self.destination_city = destination_city
+        self.destination_airport = destination_airport
+        self.out_date = out_date
+        self.return_date = return_date
+
+    # def flight_data(self, IATA_code):
+    #     # You can use the split() function to get the first part of the date from the API response.
+        
+    #     # Get the current date and time
+    #     current_date = datetime.now()
+    #     current_time = current_date.strftime("%d-%m-%Y")
+
+    #     # Add 6 intervals of 30 days
+    #     new_date_after_30_days = current_date + timedelta(days=30*6)
+    #     new_date_after_30_days = new_date_after_30_days.strftime("%d-%m-%Y")
+
+    #     # Add 6 months
+    #     new_date_after_6_months = current_date.replace(month=current_date.month + 6)
+    #     new_date_after_6_months = new_date_after_6_months.strftime("%d-%m-%Y")
+
+    #     print("Current Date:", current_date)
+    #     print("Date after 6 intervals of 30 days:", new_date_after_30_days)
+    #     print("Date after 6 months:", new_date_after_6_months)
+        
+    #     query = {
+    #         "fly_from": "SAO",
+    #         "location_types": "city",
+    #         "fly_to": IATA_code,
+    #         "date_from": current_time,
+    #         "date_to": new_date_after_30_days,
+    #     }
+        
+    #     response = requests.get(
+    #         url= f"{TEQUILA_ENDPOINT}/search",
+    #         headers = header_key, #type : ignore
+    #         params = query
+    #         )
+        
+    #     print(response.text)
--- Day 30 - 39/Day 39/main.py
+++ Day 30 - 39/Day 39/main.py
@@ -11,11 +11,12 @@ from pprint import pprint
 #Imported files
 from data_manager import DataManager
 from notification_manager import NotificationManager
-# from flight_data import FlightData
+from flight_data import FlightData
 from flight_search import FlightSearch
 
 data_manager = DataManager()
 flight_search = FlightSearch()
+flight_data = FlightData()
 sheet_data = data_manager.get_destination_data()
 
 
@@ -25,7 +26,8 @@ for row_index, row in enumerate(sheet_data[:10], start=1):  # Loop through the f
         flight_search = FlightSearch()
         for row in sheet_data:
             row["iataCode"] = flight_search.get_destination_code(row["city"])
-        # print(f"sheet_data:\n {sheet_data}")
+            row["iataCode"] = flight_data.flight_data(row["code"])
+        print(f"sheet_data:\n {sheet_data}")
 
         data_manager.destination_data = sheet_data
         data_manager.update_destination_codes()
