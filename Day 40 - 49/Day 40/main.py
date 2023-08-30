4303bff5cfde6ca54739b094facc85142a54b027
Theodoro Bertol <dev.theodoro@gmail.com>
2023-08-30 16:04:22 -0300

    Day 40 done :)

--- Day 40 - 49/Day 40/main.py
+++ Day 40 - 49/Day 40/main.py
@@ -1,24 +1,53 @@
 # Theodoro Bertol Dev (Abeelha) #
 # || Day 40 of #100DaysOfCode || #
 
-import sheety
+from datetime import datetime, timedelta
+from data_manager import DataManager
+from flight_search import FlightSearch
+from notification_manager import NotificationManager
 
-print("Welcome to Abeelha Flight CLub. \nWe find the best flight deals and email them to you.")
 
-first_name = input("What is your First Name? ")
-last_name = input("What is your Last Name? ")
+ORIGIN_CITY_IATA = "SAO"
 
-email1 = "email@email.com1"
-email2 = "email@email.com2"
+data_manager = DataManager()
+flight_search = FlightSearch()
+notification_manager = NotificationManager()
 
-while email1 != email2:
-    email1 = input("What is your email? ")
-    if email1.lower() in ["quit", "exit"]:
-        exit()
-    email2 = input("What is your email? ")
-    if email2.lower() in ["quit", "exit"]:
-        exit()
-    
-print("OK, You re in the club!")
+sheet_data = data_manager.get_destination_data()
 
-sheety.post_new_row(first_name, last_name, email1)
+if sheet_data[0]["iataCode"] == "":
+    city_names = [row["city"] for row in sheet_data]
+    data_manager.city_codes = flight_search.get_destination_codes(city_names)  # type: ignore
+    data_manager.update_destination_codes()
+    sheet_data = data_manager.get_destination_data()
+
+destinations = {
+    data["iataCode"]: {
+        "id": data["id"],
+        "city": data["city"],
+        "price": data["lowestPrice"]
+    } for data in sheet_data}
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=6 * 30)
+
+for destination_code, value in destinations.items():
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination_code,
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+    if flight is None:
+        continue
+
+    if flight.price < value["price"]:
+        message = f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+
+        ######################
+        if flight.stop_overs > 0:
+            message += f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city}."
+            print(message)
+        #######################
+
+        notification_manager.send_sms(message)
