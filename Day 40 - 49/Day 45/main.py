ff8cccfdebd9146c64b2f585a8ca93f292938a64
Theodoro Bertol <dev.theodoro@gmail.com>
2023-09-18 11:57:51 -0300

    Second commit day 45

--- Day 40 - 49/Day 45/main.py
+++ Day 40 - 49/Day 45/main.py
@@ -1,11 +1,36 @@
 # Theodoro Bertol Dev (Abeelha) #
 # || Day 45 of #100DaysOfCode || #
 
+import requests
 from bs4 import BeautifulSoup
-import os
 
-content = open('./website.html','r')
-
+with open("website.html", encoding="utf-8") as file:
+    content = file.read()
+    
 soup = BeautifulSoup(content, "html.parser")
 
 print(soup.title)
+
+response = requests.get("https://news.ycombinator.com/")
+yc_web_page = response.text
+soup = BeautifulSoup(yc_web_page, 'html.parser')
+
+article_titles = []
+article_links = []
+for article_tag in soup.find_all(name="span", class_="titleline"):
+    article_titles.append(article_tag.getText())
+    article_links.append(article_tag.find("a")["href"])
+
+article_upvotes = []
+for article in soup.find_all(name="td", class_="subtext"):
+    if article.span.find(class_="score") is None:
+        article_upvotes.append(0)
+    else:
+        article_upvotes.append(int(article.span.find(class_="score").contents[0].split()[0]))
+
+max_points_index = article_upvotes.index(max(article_upvotes))
+print(
+    f"{article_titles[max_points_index]}, "
+    f"{article_upvotes[max_points_index]} points, "
+    f"available at: {article_links[max_points_index]}."
+)
